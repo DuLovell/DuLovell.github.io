5891370daf90ee9c1aafdb6af626843f7957cd04
admin <bednyakov99@gmail.com>
2020-05-01 11:35:41 +0300

    changing method from POST to GET

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
-$image_url = $_POST['image'];
-$fio = $_POST['fio'];
-$email = $_POST['email'];
+$image_url = $GET['image'];
+$fio = $GET['fio'];
+$email = $GET['email'];
 
 
 if (mail("photoshopdemo2@gmail.com", "Заказ с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: bednyakov99@gmail.com \r\n"))
